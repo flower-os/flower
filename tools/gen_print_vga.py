715da4b035e31b58ee2cbba4cbe9a599aff3725f
gegy1000 <gegy1000@gmail.com>
2017-11-02 12:46:45 +0200

    Various changes
    E.g added newlines & added fg colour param to gen_print_vga tool

--- tools/gen_print_vga.py
+++ tools/gen_print_vga.py
@@ -1,15 +1,17 @@
 # Basic, top-line vga print asm code gen script
 
-def hex_letter(letter):
-    return "0x02" + hex(ord(letter))[2:]
-    
-def gen_asm(string):
+
+def hex_letter(letter, colour):
+    return "0x0{0}{1}".format(colour, hex(ord(letter))[2:])
+
+
+def gen_asm(string, colour):
 
     out = ""
     
     for letter, index in zip(string, range(len(string))):
     
-        code = hex_letter(letter)
+        code = hex_letter(letter, colour)
         
         if 2 * index < 10:
             fmt_str = "mov word [0xb8000 +  {0}], {1} ; {2}\n"
@@ -23,4 +25,4 @@ def gen_asm(string):
 
 while True:
 
-    print(gen_asm(input("String > ")))
+    print(gen_asm(input("String > "), input("Colour (Hex) > ")))
