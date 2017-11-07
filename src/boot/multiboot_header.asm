1bf6cad6bcef731a959b6ab27c45c9358e25a3e3
gegy1000 <gegy1000@gmail.com>
2017-11-07 09:30:17 +0200

    Fixed multiboot header comments

--- src/boot/multiboot_header.asm
+++ src/boot/multiboot_header.asm
@@ -1,16 +1,19 @@
+; Boot headers for multiboot 2
+; This lets the OS boot with any multiboot 2 compatible bootloader
+; GRUB is the chosen bootloader for its ubiquity
 
 section .multiboot_header
 
 header_start:
-    dd 0xe85250d6 
-    dd 0 
-    dd header_end - header_start 
+    dd 0xe85250d6 ; set magic number
+    dd 0 ; set protected mode
+    dd header_end - header_start ; set length of the header
 
-    
+    ; header checksum (0x100000000 - (magic number + mode + length))
     dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
     
-    
-    dw 0 
-    dw 0 
-    dd 8 
+    ; end tag
+    dw 0 ; type
+    dw 0 ; flags
+    dd 8 ; size
 header_end:
