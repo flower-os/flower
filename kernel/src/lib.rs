dce00f8a391c3981876646a7284628510b21fbe9
restioson <restiosondev@gmail.com>
2017-11-09 20:26:50 +0200

    Added VGA driver WIP
    Printing doesn't work yet -- must fix

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -5,48 +5,24 @@
 #![feature(const_fn)]
 #![feature(const_unique_new)]
 #![feature(unique)]
+#![feature(slice_rotate)]
 
 extern crate rlibc;
+extern crate volatile;
+extern crate spin;
 
 mod lang;
 mod vga;
 
-use core::ptr::write_volatile;
+use vga::Color;
 
 /// Kernel main function
 #[no_mangle]
 pub extern fn kmain() -> ! {
-    clear_screen();
-    boot_print();
+    vga::WRITER.lock().fill_screen(Color::Black);
+    vga::WRITER.lock().write_str("Flower kernel boot");
+    vga::WRITER.lock().write_char('b');
 
     unsafe { asm!("hlt"); }
     loop {}
-}
-
-// TODO could be optimized to clear 4 pixels at a time
-fn clear_screen() {
-
-    // TODO could be constants
-    let bottom_right_pixel = (25 * 80) + 80; // Pixel with highest value
-    let vga_ptr = 0xb8000 as *mut u16;
-
-    for location in 0..bottom_right_pixel {
-        unsafe {
-            write_volatile(vga_ptr.offset(location), 0)
-        }
-    }
-
-}
-
-fn boot_print() {
-
-    // TODO could be constant
-    let vga_ptr = 0xb8000 as *mut u16;
-
-    for (index, char) in b"Flower kernel boot".iter().enumerate() {
-        unsafe {
-            write_volatile(vga_ptr.offset(index as isize), 0x0200u16 | *char as u16)
-        }
-    }
-
 }
--- kernel/src/vga.rs
+++ kernel/src/vga.rs
@@ -1,7 +1,16 @@
-use core::ptr::Unique;
+use volatile::Volatile;
+use spin::Mutex;
+use core::ptr::{Unique, write_volatile};
+use core::ops::{Deref, DerefMut};
 
-const BUFFER_WIDTH: usize = 80;
-const BUFFER_HEIGHT: usize = 25;
+const VGA_ADDR: usize = 0xb8000;
+const RESOLUTION_X: usize = 80;
+const RESOLUTION_Y: usize = 25;
+
+pub static WRITER: Mutex<VgaWriter> = Mutex::new(VgaWriter::new(
+    VgaColor::new(Color::White, Color::Black)
+));
+
 
 /// Interface to VGA, allowing write
 pub struct VgaWriter {
@@ -11,22 +20,36 @@ pub struct VgaWriter {
 }
 
 impl VgaWriter {
+    const fn new(color: VgaColor) -> Self {
+        VgaWriter {
+            column_position: 0,
+            color: color,
+            buffer: unsafe { Unique::new_unchecked(0xb8000 as *mut _) },
+        }
+    }
+
     pub fn write_char(&mut self, character: char) {
         match character {
             '\n' => self.new_line(),
             character => {
-                if self.column_position >= BUFFER_WIDTH {
+                if self.column_position >= RESOLUTION_X {
                     self.new_line();
                 }
-                let row = BUFFER_HEIGHT - 1;
+                let row = RESOLUTION_Y - 1;
                 let column = self.column_position;
                 let char_color = self.color;
-                self.buffer().0[row][column] = VgaChar {
+                self.buffer()[row][column].write(VgaChar {
                     color: char_color,
                     character: character,
-                };
+                });
                 self.column_position += 1;
             }
+        }
+    }
+
+    pub fn write_str(&mut self, str: &str) {
+        for char in str.chars() {
+            self.write_char(char);
         }
     }
 
@@ -34,31 +57,91 @@ impl VgaWriter {
         unsafe { self.buffer.as_mut() }
     }
 
-    fn new_line(&mut self) {}
+    fn new_line(&mut self) {
+        if self.column_position < RESOLUTION_Y - 1 {
+            self.column_position += 1
+        } else {
+            self.buffer().rotate(1); // Rotate 1 (shift elements left 1)
+            self.clear_row(RESOLUTION_Y - 1);
+        }
+    }
+
+    // TODO clear last row only?
+    fn clear_row(&mut self, y: usize) {
+        let blank = VgaChar::new(self.color, ' ');
+
+        for column in 0..RESOLUTION_X {
+            self.buffer()[y][column].write(blank);
+        }
+    }
+
+    /// Fills the screen 4 pixels at a time
+    pub fn fill_screen(&mut self, fill_colour: Color) {
+        let blank_char = (fill_colour as u64) << 8;
+
+        let blank = (blank_char << 48) |
+            (blank_char << 32) |
+            (blank_char << 16) |
+            blank_char;
+
+        let vga_ptr = VGA_ADDR as *mut u64;
+
+        // Clear pixels four at a time
+        for four_pixels in 0..((RESOLUTION_Y * RESOLUTION_X) / 4) as isize {
+            unsafe {
+                write_volatile(vga_ptr.offset(four_pixels), blank);
+            }
+        }
+    }
 }
 
 /// Represents the complete VGA character buffer, containing a 2D array of VgaChar
-struct VgaBuffer([[VgaChar; BUFFER_WIDTH]; BUFFER_HEIGHT]);
+struct VgaBuffer([[Volatile<VgaChar>; RESOLUTION_X]; RESOLUTION_Y]);
+
+impl Deref for VgaBuffer {
+    type Target = [[Volatile<VgaChar>; RESOLUTION_X]; RESOLUTION_Y];
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for VgaBuffer {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
 
 /// Represents a full character in the VGA buffer, with a character code, foreground and background
+#[derive(Copy, Clone)]
 #[repr(C)]
 struct VgaChar {
-    color: VgaColor,
-    character: char,
+    pub color: VgaColor,
+    pub character: char,
+}
+
+impl VgaChar {
+    fn new(color: VgaColor, character: char) -> Self {
+        VgaChar {
+            color: color,
+            character: character
+        }
+    }
 }
 
 /// Represents a VGA colour, with both a foreground and background
-#[derive(Debug, Clone, Copy)]
-struct VgaColor(u8);
+#[derive(Clone, Copy)]
+pub struct VgaColor(u8);
 
 impl VgaColor {
     /// Creates a new VgaColor for the given foreground and background
-    const fn new_color(foreground: Color, background: Color) -> VgaColor {
+    pub const fn new(foreground: Color, background: Color) -> VgaColor {
         VgaColor((background as u8) << 4 | (foreground as u8))
     }
 }
 
 /// Represents valid VGA colors
+#[allow(dead_code)] // dead variants for completeness
 #[repr(u8)]
 pub enum Color {
     Black = 0,
