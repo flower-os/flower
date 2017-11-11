9ed2ab3b6ee0c8d3b1a94d33dbc78bddeaeec535
Restioson <restiosondev@gmail.com>
2017-11-11 09:05:43 +0200

    Abstracted the vga buffer more

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -6,10 +6,13 @@
 #![feature(const_unique_new)]
 #![feature(unique)]
 #![feature(slice_rotate)]
+#![feature(try_from)]
+#![feature(type_ascription)]
 
 extern crate rlibc;
 extern crate volatile;
 extern crate spin;
+
 
 mod lang;
 mod vga;
@@ -20,8 +23,7 @@ use vga::Color;
 #[no_mangle]
 pub extern fn kmain() -> ! {
     vga::WRITER.lock().fill_screen(Color::Black);
-    vga::WRITER.lock().write_str("Flower kernel boot");
-    vga::WRITER.lock().write_char('b');
+    vga::WRITER.lock().write_str("Flower kernel boot\n").expect("Writing to VGA failed");
 
     unsafe { asm!("hlt"); }
     loop {}
--- kernel/src/vga.rs
+++ kernel/src/vga.rs
@@ -1,7 +1,9 @@
 use volatile::Volatile;
 use spin::Mutex;
 use core::ptr::{Unique, write_volatile};
-use core::ops::{Deref, DerefMut};
+use core::ops::{Index, IndexMut};
+use core::cmp;
+use core::convert::{TryFrom, TryInto};
 
 const VGA_ADDR: usize = 0xb8000;
 const RESOLUTION_X: usize = 80;
@@ -19,6 +21,11 @@ pub struct VgaWriter {
     buffer: Unique<VgaBuffer>,
 }
 
+#[derive(Debug)]
+pub enum VgaWriteError {
+    ColorCodeOutOfBounds(u8)
+}
+
 impl VgaWriter {
     const fn new(color: VgaColor) -> Self {
         VgaWriter {
@@ -28,12 +35,12 @@ impl VgaWriter {
         }
     }
 
-    pub fn write_char(&mut self, character: char) {
+    pub fn write_char(&mut self, character: char) -> Result<(), VgaWriteError> {
         match character {
-            '\n' => self.new_line(),
+            '\n' => self.new_line()?,
             character => {
                 if self.column_position >= RESOLUTION_X {
-                    self.new_line();
+                    self.new_line()?;
                 }
                 let row = RESOLUTION_Y - 1;
                 let column = self.column_position;
@@ -45,35 +52,43 @@ impl VgaWriter {
                 self.column_position += 1;
             }
         }
+
+        Ok(())
     }
 
-    pub fn write_str(&mut self, str: &str) {
+    pub fn write_str(&mut self, str: &str) -> Result<(), VgaWriteError> {
         for char in str.chars() {
-            self.write_char(char);
+            self.write_char(char)?;
         }
+
+        Ok(())
     }
 
     fn buffer(&mut self) -> &mut VgaBuffer {
         unsafe { self.buffer.as_mut() }
     }
 
-    fn new_line(&mut self) {
+    fn new_line(&mut self) -> Result<(), VgaWriteError> {
+        let color = self.color;
+
         if self.column_position < RESOLUTION_Y - 1 {
             self.column_position += 1
         } else {
-            self.buffer().rotate(1); // Rotate 1 (shift elements left 1)
-            self.clear_row(RESOLUTION_Y - 1);
+            // Scroll down 1
+            self.buffer().scroll_down(1,
+                                      (color
+                                          .try_into()
+                                          .map_err(|e: ColorCodeOutOfBounds|
+                                              VgaWriteError::ColorCodeOutOfBounds(e.0)
+                                          )?: (Color, Color)).1
+            );
         }
+
+        Ok(())
     }
 
     // TODO clear last row only?
-    fn clear_row(&mut self, y: usize) {
-        let blank = VgaChar::new(self.color, ' ');
 
-        for column in 0..RESOLUTION_X {
-            self.buffer()[y][column].write(blank);
-        }
-    }
 
     /// Fills the screen 4 pixels at a time
     pub fn fill_screen(&mut self, fill_colour: Color) {
@@ -98,20 +113,47 @@ impl VgaWriter {
 /// Represents the complete VGA character buffer, containing a 2D array of VgaChar
 struct VgaBuffer([[Volatile<VgaChar>; RESOLUTION_X]; RESOLUTION_Y]);
 
-impl Deref for VgaBuffer {
-    type Target = [[Volatile<VgaChar>; RESOLUTION_X]; RESOLUTION_Y];
+impl VgaBuffer {
+    pub fn scroll_down(&mut self, amount: usize, background_color: Color) {
+        let amount = cmp::min(amount, RESOLUTION_Y);
 
-    fn deref(&self) -> &Self::Target {
-        &self.0
+        // Shift elements left by amount only if amount < Y resolution
+        // If amount is any more then the data will be cleared anyway
+        if amount != RESOLUTION_Y {
+            self.0.rotate(amount);
+        }
+
+        // Clear rows up to the amount
+        for row in 0..amount {
+            self.clear_row(row, background_color);
+        }
+    }
+
+    fn clear_row(&mut self, y: usize, color: Color) {
+        let blank = VgaChar::new(
+            VgaColor::new(Color::Black, color),
+            ' '
+        );
+
+        for column in 0..RESOLUTION_X {
+            self.0[y][column].write(blank);
+        }
     }
 }
 
-impl DerefMut for VgaBuffer {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
+impl Index<usize> for VgaBuffer {
+    type Output = [Volatile<VgaChar>; RESOLUTION_X];
+
+    fn index(&self, i: usize) -> &Self::Output {
+        &self.0[i]
     }
 }
 
+impl IndexMut<usize> for VgaBuffer {
+    fn index_mut(&mut self, i: usize) -> &mut Self::Output {
+        &mut self.0[i]
+    }
+}
 /// Represents a full character in the VGA buffer, with a character code, foreground and background
 #[derive(Copy, Clone)]
 #[repr(C)]
@@ -140,8 +182,17 @@ impl VgaColor {
     }
 }
 
+impl TryFrom<VgaColor> for (Color, Color) {
+    type Error = ColorCodeOutOfBounds;
+
+    fn try_from(color: VgaColor) -> Result<Self, Self::Error> {
+        Ok((Color::try_from(color.0 >> 4)?, Color::try_from(color.0 & 0x00FF)?))
+    }
+}
+
 /// Represents valid VGA colors
 #[allow(dead_code)] // dead variants for completeness
+#[derive(Copy, Clone)]
 #[repr(u8)]
 pub enum Color {
     Black = 0,
@@ -160,4 +211,36 @@ pub enum Color {
     Pink = 13,
     Yellow = 14,
     White = 15,
+}
+
+/// Struct to show that the color code was out of bounds for [TryFrom] for [Color]
+pub struct ColorCodeOutOfBounds(u8);
+
+impl TryFrom<u8> for Color {
+
+    /// The only type of error is out of bounds
+    type Error = ColorCodeOutOfBounds;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        use Color::*;
+        match value {
+            0 => Ok(Black),
+            1 => Ok(Blue),
+            2 => Ok(Green),
+            3 => Ok(Cyan),
+            4 => Ok(Red),
+            5 => Ok(Magenta),
+            6 => Ok(Brown),
+            7 => Ok(LightGray),
+            8 => Ok(DarkGray),
+            9 => Ok(LightBlue),
+            10 => Ok(LightGreen),
+            11 => Ok(LightCyan),
+            12 => Ok(LightRed),
+            13 => Ok(Pink),
+            14 => Ok(Yellow),
+            15 => Ok(White),
+            code => Err(ColorCodeOutOfBounds(code))
+        }
+    }
 }
