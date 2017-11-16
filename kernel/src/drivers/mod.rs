f4b473141d5ce60335ea769764ccde064957c6af
gegy1000 <gegy1000@gmail.com>
2017-11-16 15:32:39 +0200

    Fix style issues Update .gitattributes to fix \r\n in ascii art

--- kernel/src/drivers/mod.rs
+++ kernel/src/drivers/mod.rs
@@ -1,4 +1,3 @@
 #[macro_use]
 pub mod vga;
-
 pub mod ps2;
--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -18,37 +18,37 @@ mod io;
 #[macro_use]
 mod drivers;
 
-use drivers::vga::{VgaColor, Color};
+use drivers::vga::{self, VgaColor, Color};
 
-const FLOWER: &'static str = include_str!("resources/ascii_art/flower.txt");
-const FLOWER_STEM: &'static str = include_str!("resources/ascii_art/flower_stem.txt");
+const FLOWER: &'static str = include_str!("resources/art/flower.txt");
+const FLOWER_STEM: &'static str = include_str!("resources/art/flower_stem.txt");
 
 /// Kernel main function
 #[no_mangle]
 pub extern fn kmain() -> ! {
-    drivers::vga::WRITER.lock().fill_screen(Color::Black);
+    vga::WRITER.lock().fill_screen(Color::Black);
 
     // Print flower
-    drivers::vga::WRITER.lock().set_color(
+    vga::WRITER.lock().set_color(
         VgaColor::new(Color::LightBlue, Color::Black)
     );
     print!("\n{}", FLOWER);
-    drivers::vga::WRITER.lock().set_color(
+    vga::WRITER.lock().set_color(
         VgaColor::new(Color::Green, Color::Black)
     );
     print!("{}", FLOWER_STEM);
 
     // Reset colors
-    drivers::vga::WRITER.lock().set_color(
+    vga::WRITER.lock().set_color(
         VgaColor::new(Color::White, Color::Black)
     );
 
     // Reset cursor position to (0, 0)
     // It's hackish but it looks better
-    drivers::vga::WRITER.lock().set_cursor_pos((0, 0));
+    vga::WRITER.lock().set_cursor_pos((0, 0));
 
     // Print boot message
-    drivers::vga::WRITER.lock().write_str_colored(
+    vga::WRITER.lock().write_str_colored(
         "Flower kernel boot!\n-------------------\n\n",
          VgaColor::new(Color::Green, Color::Black)
     ).expect("Color code should be valid");
