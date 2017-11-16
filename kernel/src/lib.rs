686b17cadc5e2c2a716ad93ac7d2a21b245cbddb
Restioson <restiosondev@gmail.com>
2017-11-16 11:15:19 +0200

    Added flower ascii art print
    Also added git attribute to force linefeed, otherwise \r's get printed as semiquavers

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -19,11 +19,29 @@ mod vga;
 
 use vga::Color;
 
+const FLOWER: &'static str = include_str!("flower.txt");
+const FLOWER_STEM: &'static str = include_str!("flower_stem.txt");
+
 /// Kernel main function
 #[no_mangle]
 pub extern fn kmain() -> ! {
     vga::WRITER.lock().fill_screen(Color::Black);
     println!("Flower kernel boot");
+
+    // Print flower
+    vga::WRITER.lock().set_color(
+        vga::VgaColor::new(Color::LightBlue, Color::Black)
+    );
+    print!("{}", FLOWER);
+    vga::WRITER.lock().set_color(
+        vga::VgaColor::new(Color::Green, Color::Black)
+    );
+    print!("{}", FLOWER_STEM);
+
+    // Reset colors
+    vga::WRITER.lock().set_color(
+        vga::VgaColor::new(Color::White, Color::Black)
+    );
 
     unsafe { asm!("hlt"); }
     loop {}
