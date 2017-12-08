3f5d18dcc5943bed659357a2944dc52167786783
gegy1000 <gegy1000@gmail.com>
2017-12-08 16:51:56 +0200

    Add bitflags and use results for PS/2 IO

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -92,6 +92,7 @@ pub extern fn kmain() -> ! {
     } else {
         println!("kbd: enable unsuccessful");
     }
+    ps2::PS2.lock().initialize().expect("PS/2 should successfully initialize");
 
     unsafe { halt() }
 }
