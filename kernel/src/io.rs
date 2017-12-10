146d73b944d6768d12da894e606fc6d89fe185c0
too-r <william@huddie.net>
2017-12-10 11:38:49 +0000

    Add inw, outw, inl and outl abstractions

--- kernel/src/io.rs
+++ kernel/src/io.rs
@@ -10,6 +10,30 @@ pub unsafe fn outb(port: u16, value: u8) {
     asm!("outb %al, %dx" :: "{dx}"(port), "{al}"(value));
 }
 
+//Read word from port.
+pub unsafe fn inw(port: u16) -> u16 {
+    let result: u16;
+    asm!("inw %dx, %ax" : "={ax}"(result) : "{dx}"(port) :: "volatile");
+    result
+}
+
+///Write a word to the port.
+pub unsafe fn outw(value: u16, port: u16) {
+    asm!("outw %ax, %dx" :: "{dx}"(port), "{ax}"(value) :: "volatile");
+}
+
+///Read a dword from the port.
+pub unsafe fn inl(port: u16) -> u32 {
+    let result: u32;
+    asm!("inl %dx, %eax" : "={eax}"(result) : "{dx}"(port) :: "volatile");
+    result
+}
+
+///Write a dword to the port.
+pub unsafe fn outl(value: u32, port: u16) {
+    asm!("outl %eax, %dx" :: "{dx}"(port), "{eax}"(value) :: "volatile");
+}
+
 /// Represents a port to be accessed through inb and outb
 pub struct IOPort {
     port: u16,
