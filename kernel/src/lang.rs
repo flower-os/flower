7c240e38f68dd8ceefacd40077a9e2f376dd0867
gegy1000 <gegy1000@gmail.com>
2018-03-10 17:03:01 +0200

    Remove debug line

--- kernel/src/lang.rs
+++ kernel/src/lang.rs
@@ -22,7 +22,6 @@ extern fn panic_fmt(args: fmt::Arguments, file: &'static str, line: u32) -> ! {
 
     // Ignore the errors because we can't afford to panic in the panic handler
 
-    let _ = writer.set_cursor_pos(terminal::Point::new(0, 5)); // TODO no
     let _ = writer.set_color(ColorPair::new(Color::Red, Color::Black));
     let _ = write!(&mut writer, "Panicked at \"{}\", {file}:{line}\n", args, file = file, line = line);
 
